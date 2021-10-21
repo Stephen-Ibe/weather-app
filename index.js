c7d988ea9deac347c0a12b3a9b42f3fdd8f9fde6
Ibe Stephen <saintkalson@gmail.com>
2021-10-21 14:23:47 +0100

    routes setup

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv/config");
-const app = express();
 
 const PORT = process.env.PORT || 5000;
+const app = express();
 
+// routes import
+const mainRoutes = require("./routes");
+
+// enable cors
 app.use(cors());
 
-app.get("/api", (req, res) => {
-  res.json({ success: true });
-});
+// Routes
+app.use("/api", mainRoutes);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+//khensanimamoepa
