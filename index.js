f644ab9e4807d70c99f4d09a71ee6e9460052c93
Ibe Stephen <saintkalson@gmail.com>
2021-10-21 13:41:21 +0100

    server setup complete

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv/config");
+const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.json({ success: true });
+});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
